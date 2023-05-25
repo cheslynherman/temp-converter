7da87551ec1f46a87ac4a5849522b57344ce0bc5
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-05-25 11:56:52 +0200

    converter complete

---
+++ assets/index.js
@@ -0,0 +1,23 @@
+function calculate(){
+    let calculation;
+    let fahrenheit= parseInt(document.getElementById("convert").value);
+    let results= document.getElementById("results");
+
+    calculation= ((fahrenheit-32)/1.8).toFixed(2)
+    results.textContent=calculation
+    document.getElementById("results").innerHTML = output;
+}
+function convert() {
+    let calculate;
+    let celsius= parseInt(document.getElementById("converter").value);
+    let complete= document.getElementById("results-f");
+
+    calculate= ((celsius*9/5)+32).toFixed(2);
+    complete.textContent= calculate;
+}
+
+
+
+function reloadPage(){
+    window.location.reload();
+}
